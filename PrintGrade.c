e609d7394534f970fdb01b4a11b13cda29a51afa
artsrw <srw-art.t@hotmail.com>
2016-04-20 17:03:58 +0700

    No19Afix

--- PrintGrade.c
+++ PrintGrade.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 void main()
 {
-  char Name[70][40] = {"Sam Matthews", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"};
-  int ID[70] = {5301, 5302, 5303, 5304, 5305, 5306};
+  char Name[65][40] = {"Sam Matthews", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"};
+  int ID[65] = {5301, 5302, 5303, 5304, 5305, 5306};
   float GPA[30] = {2.73, 2.35, 3.65, 3.24, 1.32, 3.26};
   int N = 6, i;
   int SearchID, found_index;
